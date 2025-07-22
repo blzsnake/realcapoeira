48a67a0cebf94028da37013fd5fba89a3d7713b7
Egor Pestov <egorpestov2012@yandex.ru>
2025-07-22 11:14:31 +0400

    SchoolPhoto WIP

--- src/routes/ui/SchoolPhoto/SchoolPhoto.tsx
+++ src/routes/ui/SchoolPhoto/SchoolPhoto.tsx
@@ -1,15 +1,30 @@
+import { Button } from '~shared/button/Button';
 import styles from './SchoolPhoto.module.css';
 import AllCrew from '../../../app/assets/AllCrew.png';
 
 export function SchoolPhoto() {
   return (
     <div className={styles.SchoolPhoto}>
-      <h1 className={styles.Heading}>
-        ШКОЛА
-        <br />
-        REAL CAPOEIRA
-      </h1>
+      <div className={styles.HeadingBlock}>
+        <h1 className={styles.Heading}>
+          ШКОЛА
+          <br />
+          REAL CAPOEIRA
+        </h1>
+        <div className={styles.ButtonsBlockMobile}>
+          <Button className={styles.Button}>О школе</Button>
+          <Button color="white" className={styles.Button}>
+            Филиалы
+          </Button>
+        </div>
+      </div>
       <img src={AllCrew} className={styles.Image} alt="Команда" />
+      <div className={styles.ButtonsBlockWeb}>
+        <Button className={styles.Button}>О школе</Button>
+        <Button color="white" className={styles.Button}>
+          Филиалы
+        </Button>
+      </div>
     </div>
   );
 }
--- src/shared/button/types.ts
+++ src/shared/button/types.ts
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 
-type TButtonColor = 'black' | 'yellow';
+type TButtonColor = 'black' | 'yellow' | 'white';
 
 export type TButtonProps = {
   color?: TButtonColor;
