79c4f9786afa9c4fbdcd4dfdbbeeaddc87984e56
Egor Pestov <egorpestov2012@yandex.ru>
2025-07-23 14:15:29 +0400

    About initial

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { SchoolPhoto } from './ui/SchoolPhoto';
 import { Events } from './ui/Events';
+import { About } from './ui/About';
 import styles from './index.module.css';
 
 export function IndexPage() {
@@ -8,6 +9,7 @@ export function IndexPage() {
       <SchoolPhoto />
       <div className={styles.Content}>
         <Events />
+        <About />
       </div>
     </main>
   );
