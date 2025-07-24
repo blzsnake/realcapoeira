4351ef01cc0f0a824c107fed1885b17d8f379640
Egor Pestov <egorpestov2012@yandex.ru>
2025-07-24 16:18:16 +0400

    Groups WIP ховеры

--- src/routes/ui/Groups/Groups.tsx
+++ src/routes/ui/Groups/Groups.tsx
@@ -13,43 +13,51 @@ export function Groups() {
       </Typography>
       <div className={styles.Grid}>
         <div className={styles.Card}>
-          <Typography className={styles.Title} weight="demiBold">
-            Дети 3-6 лет
-          </Typography>
-          <Typography className={styles.Description}>
-            Базовые упражнения для развития координации и гибкости учат малышей
-            слышать свое тело
-          </Typography>
+          <div className={styles.InfoWrap}>
+            <Typography className={styles.Title} weight="demiBold">
+              Дети 3-6 лет
+            </Typography>
+            <Typography className={styles.Description}>
+              Базовые упражнения для развития координации и гибкости учат
+              малышей слышать свое тело
+            </Typography>
+          </div>
           <img src={Baby} alt="Дети 3-6 лет" className={styles.Image} />
         </div>
         <div className={styles.Card}>
-          <Typography className={styles.Title} weight="demiBold">
-            Дети 7–10 лет
-          </Typography>
-          <Typography className={styles.Description}>
-            Упражнения помогают выпустить избыток энергии, стать сильнее и
-            собраннее
-          </Typography>
+          <div className={styles.InfoWrap}>
+            <Typography className={styles.Title} weight="demiBold">
+              Дети 7–10 лет
+            </Typography>
+            <Typography className={styles.Description}>
+              Упражнения помогают выпустить избыток энергии, стать сильнее и
+              собраннее
+            </Typography>
+          </div>
           <img src={Kid} alt="Дети 7–10 лет" className={styles.Image} />
         </div>
         <div className={styles.Card}>
-          <Typography className={styles.Title} weight="demiBold">
-            Дети 11–15 лет
-          </Typography>
-          <Typography className={styles.Description}>
-            Ученики знакомятся с историей капоэйры и основами музыки. Осваивают
-            технику ударов и акробатические элементы
-          </Typography>
+          <div className={styles.InfoWrap}>
+            <Typography className={styles.Title} weight="demiBold">
+              Дети 11–15 лет
+            </Typography>
+            <Typography className={styles.Description}>
+              Ученики знакомятся с историей капоэйры и основами музыки.
+              Осваивают технику ударов и акробатические элементы
+            </Typography>
+          </div>
           <img src={Teen} alt="Дети 11–15 лет" className={styles.Image} />
         </div>
         <div className={styles.Card}>
-          <Typography className={styles.Title} weight="demiBold">
-            Взрослые и подростки 16+
-          </Typography>
-          <Typography className={styles.Description}>
-            Занятия глубже знакомят с культурой капоэйры и смыслом песен, а еще
-            помогают стать увереннее в себе и смотреть на мир шире
-          </Typography>
+          <div className={styles.InfoWrap}>
+            <Typography className={styles.Title} weight="demiBold">
+              Взрослые и подростки 16+
+            </Typography>
+            <Typography className={styles.Description}>
+              Занятия глубже знакомят с культурой капоэйры и смыслом песен, а
+              еще помогают стать увереннее в себе и смотреть на мир шире
+            </Typography>
+          </div>
           <img
             src={Adult}
             alt="Взрослые и подростки 16+"
