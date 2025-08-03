6707478a3565d78c6445cad07633d588fe986c35
Egor Pestov <egorpestov2012@yandex.ru>
2025-08-03 18:01:13 +0400

    Контакты WIP

--- src/routes/contacts/index.tsx
+++ src/routes/contacts/index.tsx
@@ -1,11 +1,27 @@
-import styles from './index.module.css';
+import { Typography } from '~shared/ui/typography';
+import styles from './Contacts.module.css';
 
 export function ContactsPage() {
   return (
-    <main>
-      <p>Contacts</p>
+    <main className={styles.Wrap}>
+      <div className={styles.MainWrap}>
+        <div className={styles.Content}>
+          <Typography weight="medium" className={styles.Title}>
+            КОНТАКТЫ
+          </Typography>
+        </div>
+        <div className={styles.Form}>
+          <p>Форма</p>
+        </div>
+      </div>
     </main>
   );
 }
+
+ContactsPage.seo = {
+  metaTags: {
+    title: 'Контакты Real Capoeira',
+  },
+};
 
 export default ContactsPage;
