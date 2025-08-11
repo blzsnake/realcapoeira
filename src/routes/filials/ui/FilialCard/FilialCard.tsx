428eb35383e8c12f669ceaa74d0ffd491ad0d8ce
Alex D <dudnikov.a@chibbis.ru>
2025-08-11 21:37:19 +0300

    add filials page

--- src/routes/filials/ui/FilialCard/FilialCard.tsx
+++ src/routes/filials/ui/FilialCard/FilialCard.tsx
@@ -1,6 +1,12 @@
 import { Typography } from '~shared/ui/typography';
 import styles from './FilialCard.module.css';
+import { WEEK_DAYS } from '../../mock';
 // import { ContactsPart } from './ui/ContactsPart';
+
+type ScheduleItem = {
+  group: string;
+  time: string;
+};
 
 export type TFilialCardData = {
   address: {
@@ -17,6 +23,7 @@ export type TFilialCardData = {
     name: string;
     phone: string;
   }[];
+  schedule: ScheduleItem[][];
 };
 
 export function FilialCard(props: TFilialCardData) {
@@ -35,6 +42,22 @@ export function FilialCard(props: TFilialCardData) {
             key={coach.phone}
           >{`${coach.name} ${coach.phone}`}</Typography>
         ))}
+      </div>
+      <div className={styles.ScheduleWrap}>
+        <div className={styles.ScheduleToggle}>Посмотреть расписание</div>
+        <div className={styles.Schedule}>
+          {props?.schedule?.map((item, index) => (
+            <div className={styles.ScheduleItem}>
+              <div className={styles.ScheduleWeekDay}>{WEEK_DAYS[index]}</div>
+              <div className={styles.ScheduleGroups}>
+                {item?.map((el) => (<>
+                <div>{el.group}</div>
+                <div>{el.time}</div>
+                </>))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
