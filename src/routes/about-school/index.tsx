264fa5aeca278563c494178cb95374fe01df4ae4
Egor Pestov <egorpestov2012@yandex.ru>
2025-08-23 14:17:06 +0400

    Фиксы

--- src/routes/about-school/index.tsx
+++ src/routes/about-school/index.tsx
@@ -1,10 +1,10 @@
-import { Typography } from '~shared/ui/typography';
+import { SignUpFormGroup } from '~shared/ui/SignUpFormGroup';
+import { HowToPrepair } from './ui/HowToPrepair/HowToPrepair';
 import { HeaderPart } from './ui/HeaderPart';
 import { AgesActivities } from './ui/AgesActivities';
 import { FilialsByAges } from './ui/FilialsByAges';
 import { AboutUs } from './ui/AboutUs';
 import styles from './AboutSchool.module.css';
-import { HowToPrepair } from './ui/HowToPrepair/HowToPrepair';
 
 export function AboutSchoolPage() {
   return (
@@ -21,6 +21,13 @@ export function AboutSchoolPage() {
       </div>
       <div className={styles.Content}>
         <HowToPrepair />
+      </div>
+      <div className={styles.ContentWithoutPadding}>
+        <SignUpFormGroup
+          title="Запишитесь за пару минут"
+          description="Позвоните или оставьте заявку — тренер ответит на все вопросы и подберет подходящую группу для вас или ребенка"
+          phone="+7 (925) 555 00 77"
+        />
       </div>
     </main>
   );
