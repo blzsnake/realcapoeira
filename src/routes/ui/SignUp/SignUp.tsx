d33debd67fad9b45940d277a0eb87d9bb2ce3da4
Egor Pestov <egorpestov2012@yandex.ru>
2025-08-24 15:37:50 +0400

    Фиксы

--- src/routes/ui/SignUp/SignUp.tsx
+++ src/routes/ui/SignUp/SignUp.tsx
@@ -10,16 +10,21 @@ export function SignUp() {
       <Typography weight="demiBold" className={styles.Heading} component="h2">
         Первое занятие — бесплатно
       </Typography>
-      <Typography className={styles.Subheading}>
-        Начните свой путь к силе, ловкости и мастерству
-      </Typography>
+
       <div className={styles.Column}>
-        <img src={DreamTeam} className={styles.Image} alt="Команда" />
-        <SignUpFormGroup
-          title="Запишитесь за пару минут"
-          description="Позвоните или оставьте заявку — тренер ответит на все вопросы и подберет подходящую группу для вас или ребенка"
-          phone="+7 (925) 555 00 77"
-        />
+        <Typography className={styles.Subheading}>
+          Начните свой путь к силе, ловкости и мастерству
+        </Typography>
+        <div className={styles.ImageWrap}>
+          <img src={DreamTeam} className={styles.Image} alt="Команда" />
+        </div>
+        <div className={styles.FormWrap}>
+          <SignUpFormGroup
+            title="Запишитесь за пару минут"
+            description="Позвоните или оставьте заявку — тренер ответит на все вопросы и подберет подходящую группу для вас или ребенка"
+            phone="+7 (925) 555 00 77"
+          />
+        </div>
       </div>
     </div>
   );
