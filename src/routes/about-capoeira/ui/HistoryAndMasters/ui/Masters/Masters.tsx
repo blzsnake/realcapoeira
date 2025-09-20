ca58ef034f24c8e18056d8e1ff807806a298cfdf
Egor Pestov <egorpestov2012@yandex.ru>
2025-09-20 19:49:30 +0400

    WIP

--- src/routes/about-capoeira/ui/HistoryAndMasters/ui/Masters/Masters.tsx
+++ src/routes/about-capoeira/ui/HistoryAndMasters/ui/Masters/Masters.tsx
@@ -1,5 +1,46 @@
+import { useRef } from 'react';
+import { Typography } from '~shared/ui/typography';
+import RightArrow from '~app/assets/right_arrow.svg?react';
+import LeftArrow from '~app/assets/left_arrow.svg?react';
 import styles from './Masters.module.css';
 
 export function Masters() {
-  return <div className={styles.Masters}>masters</div>;
+  const refMasters = useRef<HTMLDivElement>(null);
+  const handleClickToScroll = (data: number) => () =>
+    refMasters?.current?.scrollBy({
+      left: data,
+      behavior: 'smooth',
+    });
+
+  return (
+    <article className={styles.Masters}>
+      <div className={styles.HeadingRow}>
+        <Typography
+          component="h3"
+          weight="demiBold"
+          className={styles.Title}
+          color="white"
+        >
+          Великие мастера
+        </Typography>
+        <div className={styles.Arrows}>
+          <LeftArrow
+            onClick={handleClickToScroll(-400)}
+            width={46}
+            height={46}
+            className={styles.Arrow}
+          />
+          <RightArrow
+            onClick={handleClickToScroll(400)}
+            width={46}
+            height={46}
+            className={styles.Arrow}
+          />
+        </div>
+      </div>
+      <div ref={refMasters} className={styles.Slider} id="#masters">
+        <p>карточка тренера</p>
+      </div>
+    </article>
+  );
 }
