93345597d77c966ad80ec87d7def1ac2935c4ae4
Egor Pestov <egorpestov2012@yandex.ru>
2025-09-27 19:25:15 +0400

    WIP

--- src/routes/about-capoeira/ui/CapoeiraComponents/CapoeiraComponents.tsx
+++ src/routes/about-capoeira/ui/CapoeiraComponents/CapoeiraComponents.tsx
@@ -17,7 +17,16 @@ export function CapoeiraComponents() {
         Из чего состоит капоэйра
       </Typography>
       <div className={styles.ComponentsContainer}>
-        <div className={styles.Component}>
+        <div
+          className={styles.Component}
+          onClick={() =>
+            window.open(
+              'https://www.youtube.com/',
+              '_blank',
+              'noopener,noreferrer'
+            )
+          }
+        >
           <div className={styles.Column}>
             <Typography weight="demiBold" className={styles.CardTitle}>
               Искусство боя
@@ -28,6 +37,69 @@ export function CapoeiraComponents() {
             </Typography>
           </div>
           <img src={Pic1} className={styles.Image} alt="Искусство боя" />
+        </div>
+        <div
+          className={styles.Component}
+          onClick={() =>
+            window.open(
+              'https://www.youtube.com/',
+              '_blank',
+              'noopener,noreferrer'
+            )
+          }
+        >
+          <div className={styles.Column}>
+            <Typography weight="demiBold" className={styles.CardTitle}>
+              Акробатика
+            </Typography>
+            <Typography className={styles.Text}>
+              Благодаря ей единоборство похоже на грациозный танец. На занятиях
+              сначала обучают простым приемам, постепенно переходя к сложным
+            </Typography>
+          </div>
+          <img src={Pic2} className={styles.Image} alt="Акробатика" />
+        </div>
+        <div
+          className={styles.Component}
+          onClick={() =>
+            window.open(
+              'https://www.youtube.com/',
+              '_blank',
+              'noopener,noreferrer'
+            )
+          }
+        >
+          <div className={styles.Column}>
+            <Typography weight="demiBold" className={styles.CardTitle}>
+              Музыка и танец
+            </Typography>
+            <Typography className={styles.Text}>
+              Во время занятий играют традиционную афро-бразильскую музыку. Она
+              помогает войти в ритм и отдает дань истории капоэйры
+            </Typography>
+          </div>
+          <img src={Pic3} className={styles.Image} alt="Искусство боя" />
+        </div>
+        <div
+          className={styles.Component}
+          onClick={() =>
+            window.open(
+              'https://www.youtube.com/',
+              '_blank',
+              'noopener,noreferrer'
+            )
+          }
+        >
+          <div className={styles.Column}>
+            <Typography weight="demiBold" className={styles.CardTitle}>
+              Рода
+            </Typography>
+            <Typography className={styles.Text}>
+              Игра, или единоборство, происходит в кругу зрителей и музыкантов —
+              ро́де. Они стоят или сидят, наблюдая за поединком
+            </Typography>
+          </div>
+          <img src={Pic4} className={styles.Image} alt="Искусство боя" />
         </div>
       </div>
     </div>
