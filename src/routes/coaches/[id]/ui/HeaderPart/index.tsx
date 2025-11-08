3785ea79f27f8feb8a1970035005125ab3dccec1
Egor Pestov <egorpestov2012@yandex.ru>
2025-11-08 19:02:47 +0400

    WIP

--- src/routes/coaches/[id]/ui/HeaderPart/index.tsx
+++ src/routes/coaches/[id]/ui/HeaderPart/index.tsx
@@ -1,10 +1,12 @@
-import cn from 'classnames';
 import { useMemo } from 'react';
 import { useRoute } from '@tramvai/module-router';
 import { Typography } from '~shared/ui/typography';
 import { Button } from '~shared/ui/button/Button';
 import { CoachAvatar } from 'src/routes/coaches/ui/CoachAvatar';
 import { COACHES } from '~shared/mocks/coaches';
+import Telegram from '~app/assets/socialIcons/Telegram.svg?react';
+import Whatsapp from '~app/assets/socialIcons/Whatsapp.svg?react';
+import Vk from '~app/assets/socialIcons/Vk.svg?react';
 import { COACH_PHOTOS } from '../../../utils';
 import styles from './HeaderPart.module.css';
 
@@ -83,6 +85,51 @@ export function HeaderPart() {
             >
               {name}
             </Typography>
+          </div>
+          <div className={styles.DesktopButtons}>
+            <div className={styles.DesktopMainButtons}>
+              <Button
+                className={styles.DesktopButton}
+                color="yellow"
+                onClick={handleScrollToForm}
+              >
+                Записаться
+              </Button>
+              <Button
+                className={styles.DesktopButton}
+                color="white"
+                url={`/filials/?coach=${coachId}`}
+                target="_self"
+              >
+                Филиалы
+              </Button>
+            </div>
+            <div className={styles.DesktopSocials}>
+              <a
+                className={styles.SocialLink}
+                href={coach.links.tg}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Telegram />
+              </a>
+              <a
+                className={styles.SocialLink}
+                href={coach.links.wa}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Whatsapp />
+              </a>
+              <a
+                className={styles.SocialLink}
+                href={coach.links.vk}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Vk />
+              </a>
+            </div>
           </div>
         </div>
         <div className={styles.CoachAvatarWrapper}>
